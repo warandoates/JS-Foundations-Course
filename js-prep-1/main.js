4a7ab6fe96d63bfeff8f862a3b91f83b30103f44
Michael Martinez <michael.martinez@galvanize.com>
2017-11-30 16:12:53 -0800

    Add files via upload

---
+++ js-prep-1/main.js
@@ -0,0 +1,88 @@
+/*
+Below are a number of problems for you to solve
+using JS. The JS code can be written below each
+problem.
+
+=======================================================
+*/
+
+
+// 1. Connect the main.js document to the HTML page.
+//    Then Update the Text to Green by adding the .status-good class.
+
+
+
+// When the JS is connected properly, the following code will update the status message.
+
+
+// 2. Declare a new variable called 'feelings',
+//    Then store a string with a word that describes
+//    how well you feel about this course material.
+//    Then display that string within the HTML page.
+
+
+
+
+// 3. Declare a new variable called 'name',
+//    store your name within it, and then
+//    create a 'message' variable with a greeting,
+//    concat the message and name variables
+//    and then display it within the HTML page.
+
+
+
+
+// 4. Declare two variables with number values.
+//    Add, subtract, multiply and divide them.
+//    Display the values of each within the HTML page.
+//    Then label these values the best you can.
+
+
+
+// 5. Declare two variables with string values.
+//    Create a third variable that concatenates the two strings that also places the appropriate space between the strings.
+//    Display the value of the third variable within the HTML page.
+
+
+
+
+// 6a. Declare a variable whose value is an empty array.
+//    Use any method you choose to add at least 4 items to it and display the array information.
+
+
+// 6b. Add an additional item to the beginning of your array.
+
+
+// For the next two problems, it may be helpful to look up Array.splice()
+// 6c. Remove the second and third items in your array and
+
+
+
+// 6d. Add two new items after the second item.
+
+
+
+// 7. When a button is clicked, add content into the ID "q7".
+
+
+
+
+// 8. Collect a number value from the user and when a button is clicked,
+//    then double user's answer and display that value into the HTML page.
+
+
+
+// 9. Set the variable 'num1' to 5.
+//    Set the variable 'num2' to 10.
+//    Test to see if num1 is greater than num2.
+//    If so, output to the HTML page: Yes, the value of num1 is greater than num2
+//    If not, output to the HTML page: No, the value of num1 is not greater than num2
+
+
+
+// 10. Set the variable 'score' to a number between 1-100.
+//    Create the following conditions:
+//    If the score is above 90, output to the HTML page "Great job".
+//    If the score is between 60 and 89, output to the HTML page "Nice work, keep practicing".
+//    If the score is below 59, output to the HTML page "Very bad: study more and retake the test".
+//    Test to make sure each condition is met properly.
