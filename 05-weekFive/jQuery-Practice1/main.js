4ef4344e6fd4fd4d1b1b0d422f3a7388a0d95825
Michael Martinez <michael.martinez@galvanize.com>
2017-12-11 13:30:35 -0800

    Add files via upload

---
+++ 05-weekFive/jQuery-Practice1/main.js
@@ -0,0 +1,138 @@
+/*
+=======================================================
+
+ ** Week 5 - Project 1  **
+*** Exercise the jQuery ***
+
+We'll be using the html code within the index.html for the
+following jQuery exercises. After you've completed an exercise,
+comment out your code (unless otherwise indicated) as these
+techniques may conflict with each other.
+
+=======================================================
+*/
+
+// Add your JS here.
+
+
+
+// 1. Make sure that the HTML document is fully loaded before running any jQuery code that is needed below. Make sure you don't comment out this code as you'll wrap this code around all the following methods (so don't forget the closing tags, too.
+// $(document).ready(function() {
+//
+// })
+
+
+// 2. Hide all of the paragraphs on the index.html page.
+
+// $(document).ready(() => {
+//   $('p').hide();
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Hide all of the h2's on the page.
+
+
+// $(document).ready(function() {
+//   $('h2').hide();
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// 4. Hide all images on the page.
+
+
+
+// $(document).ready(() => {
+//   $('html').hide();
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5. Hide all links on the page.
+
+// $(document).ready(function(){
+//   $('a').hide();
+// })
+//
+// // 6. Hide all of the elements in the document.
+//
+// $('document').ready(function(){
+//   $('html').hide();
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+// // 7. Hide all paragraphs when one of them is clicked.
+//
+// $(document).ready(function() {
+//   console.log('this will appear before click')
+//     $('p').click(function() {
+//       $('p').hide()
+//     })
+// });
+//
+//
+//
+// // 8. Hide all images when one of them is clicked.
+//
+//
+// // 9. When the user clicks onto the image, toggle the paragraphs between hide and show.
+//
+// document.getElementsByTagName('img')[0].addEventListener('click', ()=> {
+//   let elemenet = document.getElementsByTagName('p');
+//   for (var i = 0; i < element.length; i++) {
+//     if(element[i].className = 'hidden') {
+//       element[i].classList.remove('hidden')
+//     } else {
+//             element[i].classList.add('hidden')
+//     }
+//   }
+// })
