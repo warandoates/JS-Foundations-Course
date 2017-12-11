dc520918f3c40798349d4b532ecaa1cc08e5a68a
Michael Martinez <michael.martinez@galvanize.com>
2017-12-11 09:56:00 -0800

    Add files via upload

---
+++ 04-weekFour/drumKit/index.js
@@ -0,0 +1,30 @@
+function main() {
+  window.addEventListener('keydown', function(event){
+    let audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+    if (!audio) {
+      return;
+    }
+    let key = document.querySelector(`.key[data-key='${event.keyCode}']`);
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+    const keys = document.getElementsByClassName('key');
+    for(let i = 0; i < keys.length; i++) {
+      let currentKey = keys[i];
+      currentKey.addEventListener('transitionend', removeClassFunction)
+    }
+
+  });
+}
+
+main();
+
+
+function removeClassFunction(evt) {
+  if(evt.propertyName !== 'transform') {
+    return;
+  }
+  let key = evt.target;
+  key.classList.remove('playing');
+
+}
